5bf0d5b3db55c7a73e147b34e04266b304ceca90
MC-HAWK <39173095+MC-HAWK@users.noreply.github.com>
2018-05-11 18:34:08 -0400

    Add files via upload

--- weapons.py
+++ weapons.py
@@ -4,4 +4,15 @@ class weapons(items):
         super().__init__(name, description, value)
 
     def __str(self):
-        return "{}\n=====\n{}value: {}\ndamage: {}".format(self.name, self.description, self.value, self.damage)    
+        return "{}\n=====\n{}value: {}\ndamage: {}".format(self.name,
+                                                           self.description,
+                                                           self.value,
+                                                           self.damage)
+
+
+class shovel(weapons):
+    def __init__(self):
+        super().__init__(name="Shovel",
+                         description="Useful for digging and not much else.",
+                         value=3,
+                         damage=5)
