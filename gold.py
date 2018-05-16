f96dd47895c16bdc84a4d8fd9b4741854bd3e4b7
Keegan McMahon <khawkmcmahon@gmail.com>
2018-05-16 13:44:56 -0400

    Removal of gold and weapons class
    removed gold and weapons as they were not need and are included in the items class

--- gold.py
+++
@@ -1,7 +0,0 @@
-class Gold(Items):
-    def __init__(self, amt):
-        self.amt = amt
-        super().__init__(name="Gold",
-                         description="A round  coin with {} stamped on the front."
-                         .format(str(self.amt)),
-                         value=self.amt)
